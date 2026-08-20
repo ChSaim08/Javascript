a394536aac6c669870a65bf22f1a0d9189bc195c
Saim <saimulhassan114@gmail.com>
2026-08-20 09:36:32 +0500

    add index file

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+const arr = [100, 120 , 140 ,160 , 180];
+for (let i =0; i < arr.length; i++) {
+    offer= arr[i]-(0.1*arr[i]);
+    console.log(arr[i]);
+    console.log(offer);
+}
